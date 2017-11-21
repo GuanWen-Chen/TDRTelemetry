63f6f80056cb5280a0aba862c41531278ee2d772
Kevin Chen <kechen@mozilla.com>
2017-11-21 15:33:09 +0800

    Change data format.

--- js/tdr.js
+++ js/tdr.js
@@ -1,4 +1,5 @@
-var latestVersion = 59;
+var latestVersion = 59
+var shownVerNum = 3
 var tdrLog = "&graphics_critical_error=~Detected%20device%20reset&graphics_critical_error=~D3D11%20skip%20BeginFrame%20with%20with%20device-removed&graphics_critical_error=~D3D11%20detected%20a%20device%20reset"
 var reportLink = "https://crash-stats.mozilla.com/signature/?_sort=-date&signature="
 var superAPILink = "https://crash-stats.mozilla.com/api/SuperSearch/?product=Firefox"
@@ -13,15 +14,15 @@ function drawLineGraph(json, divId) {
         dates.push(date)
     }
     dates.sort();
-    console.log(dates)
     for (version in json) {
         let localMap = {};
         localMap['label'] = "Firefox" + version;
         let dataArray = [];
+        if (parseInt(version) < latestVersion - shownVerNum + 1)
+            continue
         for (d in dates) {
             dateStr = []
             dateStr = dates[d].split("-")
-            console.log(dateStr[0] + "~~~~" + dateStr[1] + "~~~~~~" + parseInt(dateStr[2]))
             plotDate = new Date(parseInt(dateStr[0]), parseInt(dateStr[1]), parseInt(dateStr[2]))
             plotDate.setDate(plotDate.getDate() - 29)
             dataArray.push([plotDate.getTime(), json[version][dates[d]]]);
@@ -69,79 +70,57 @@ function drawNormalLineGraph() {
 }
 
 function drawCrashLineGraph() {
-    json = {}
-
     var onSuccess = function(json) {
-        versionTrend[formatJsDate(curDate)] = json['total']
-    };
-    var onTDRSuccess = function(json) {
-        if (versionTrend[formatJsDate(curDate)] != 0) {
-            versionTrend[formatJsDate(curDate)] = 100.0 * parseFloat(json['total']) / parseFloat(versionTrend[formatJsDate(curDate)]);
-        }
-        console.log(versionTrend[formatJsDate(curDate)])
-    };
-
-
+            drawLineGraph(json, "#crashLineGraph");
+        };
     var onError = function (xhr, textStatus, errorThrown) {
         console.log(textStatus);
         console.log(errorThrown);
     }
+    $.ajax({
+        url: "json/crashLine.json",
+        dataType: 'json',
+        beforeSend: function(xhr) {
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("application/json");
+            }
+        },
+    })
+    .done(onSuccess)
+    .error(onError);
+}
 
-    for (let version = latestVersion - 2; version < latestVersion + 1; version++) {
-
-        curDate = new Date()
-        curDate.setDate(curDate.getDate() - timeWindow)
-        console.log("Version :" + version)
-        versionTrend = {}
-        for (let i = 0; i < timeWindow; i++) {
-            console.log("Date " + formatJsDate(curDate))
-            curDate.setDate(curDate.getDate() + 1)
-            $.ajax({
-                url: getSuperAPILink(curDate, version),
-                type: "get",
-                dataType: 'json',
-                async: false,
-            })
-            .done(onSuccess)
-            .error(onError);
-
-            $.ajax({
-                url: getSuperAPILink(curDate, version, true),
-                type: "get",
-                dataType: 'json',
-                async: false,
-            })
-            .done(onTDRSuccess)
-            .error(onError);
-
-        }
-        console.log(versionTrend)
-        json[version] = versionTrend
-    }
-    drawLineGraph(json, "#crashLineGraph");
+function createTableTitle(table) {
+    var row = table.insertRow(-1);
+    var cellsig = row.insertCell(0);
+    var cellnum = row.insertCell(1);
+    var cellper = row.insertCell(2);
+    cellsig.innerHTML = "Signature";
+    cellnum.innerHTML = "TRD Number";
+    cellper.innerHTML = "Percentage";
+    cellsig.style.backgroundColor = "#9999ff";
+    cellnum.style.backgroundColor = "#9999ff";
+    cellper.style.backgroundColor = "#9999ff";
+    cellsig.style.width = "80%";
+    cellnum.style.textAlign = "center";;
+    cellper.style.textAlign = "center";;
 }
 
 function showTDRCrashes() {
     var onSuccess = function(json) {
-        latestVersion = 58
+        let body = document.getElementById("body");
         for (version in json) {
-            table = document.getElementById("sig"+version)
+            if (parseInt(version) < latestVersion - shownVerNum + 1)
+                continue
+            let header = document.createElement("h5");
+            header.innerText = "Top 10 Signature in Firefox" + version
+            body.appendChild(header)
+
+            let table = document.createElement("table");
             if (version < latestVersion - 2)
                 continue;
             count = 0
-            var row = table.insertRow(-1);
-            var cellsig = row.insertCell(0);
-            var cellnum = row.insertCell(1);
-            var cellper = row.insertCell(2);
-            cellsig.innerHTML = "Signature";
-            cellnum.innerHTML = "TRD Number";
-            cellper.innerHTML = "Percentage";
-            cellsig.style.backgroundColor = "#9999ff";
-            cellnum.style.backgroundColor = "#9999ff";
-            cellper.style.backgroundColor = "#9999ff";
-            cellsig.style.width = "80%";
-            cellnum.style.textAlign = "center";;
-            cellper.style.textAlign = "center";;
+            createTableTitle(table)
 
             for (sig in json[version]) {
                 if (count >= 10) break;
@@ -161,6 +140,8 @@ function showTDRCrashes() {
                 cell3.style.textAlign = "center";
                 console.log(sig + " : " + json[version][sig]['count'] + " | " +  json[version][sig]['term'])
             }
+
+            body.appendChild(table)
         }
     };
     var onError = function (xhr, textStatus, errorThrown) {
@@ -170,7 +151,12 @@ function showTDRCrashes() {
 
     $.ajax({
         url: "json/crashReports.json",
-        dataType: 'jsonp',
+        dataType: 'json',
+        beforeSend: function(xhr) {
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("application/json");
+            }
+        },
     })
     .done(onSuccess)
     .error(onError);
@@ -181,4 +167,4 @@ drawNormalLineGraph();
 
 drawCrashLineGraph();
 
-//showTDRCrashes();
+showTDRCrashes();
--- python-chart.py
+++ python-chart.py
@@ -1,89 +1,17 @@
 import datetime
-from threading import Thread
 import json
 import collections
 import requests
 import sys
-import plotly.plotly as py
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
 import time
 
 urlStr = "https://crash-stats.mozilla.com/api/SuperSearch/?product=Firefox"
 tdrLogStr = "&graphics_critical_error=~Detected%20device%20reset&graphics_critical_error=~D3D11%20skip%20BeginFrame%20with%20with%20device-removed&graphics_critical_error=~D3D11%20detected%20a%20device%20reset"
-latestVersion = 58
-
-def ShowCrashRate():
-    res = requests.get("http://localhost:8888/files/output/tdrHistogram.json")
-    #print res.text
-    obj = json.loads(res.text)
-    avg = float(obj['tdrPings']) / float(obj['sessions']['count']);
-    totalTDRs = 0
-    for result in obj['results']:
-        totalTDRs += result;
-    avgTDR = float(totalTDRs)  / float(obj['tdrPings'])
-    print str(avg) + "/" + str(avgTDR)
-
-def ShowTDRPieChart():
-    res = requests.get("http://localhost:8888/files/output/tdr-statistics.json")
-    obj = json.loads(res.text)
-    #print res.text
-    labels = []
-    values = []
-    print obj['sessions']['share']
-    for key in obj['sessions']['share']:
-        labels.append(key)
-        values.append(obj['sessions']['share'][key])
-    plt.pie(values, labels=labels)
-    plt.axis('equal')
-    plt.show()
-
-def DrawLineGraph(obj, num):
-    xAxis = []
-    plt.figure(num)
-    for key in obj:
-        xAxis.append(key)
-    for version in obj[obj.keys()[0]]:
-        data = []
-        for key in obj:
-            data.append(obj[key][version])
-        plt.plot(xAxis, data, label="Firefox"+str(version))
-        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-                   ncol=5, mode="expand", borderaxespad=0.)
-    plt.show()
+latestVersion = 59
 
 def SaveJson(data, name):
     with open("json/"+name, 'w+') as fp:
         json.dump(data, fp)
-
-def ShowLineGraph():
-    latestVersion = 58
-    res = requests.get("http://localhost:8888/files/output/tdrHistogram.json")
-    obj = json.loads(res.text)
-
-    dates = {}
-    for key in obj['tdr']:
-        date = key.split(" ")
-        localDict = {}
-        for version in obj['total'][key]:
-            if int(version) < latestVersion - 3:
-                continue
-            if version not in obj['tdr'][key]:
-                tdrNum = 0
-            else:
-                tdrNum = float(obj['tdr'][key][version])
-            localDict[version] = (tdrNum/float(obj['total'][key][version]))*100
-        dates[date[0]] = localDict
-
-    SaveJson(dates, "normalLine.json")
-    dates = collections.OrderedDict(sorted(dates.items()))
-    for date in dates:
-        print str(date)
-        dates[date] = collections.OrderedDict(sorted(dates[date].items()))
-        for version in dates[date]:
-            print version + " : " + str(dates[date][version])
-    DrawLineGraph(dates, 1)
-
 
 def GetCrashNum(date, version):
     preDate = date - datetime.timedelta(1)
@@ -119,69 +47,30 @@ def GetSigNum(version, sig):
     verStr = "&version=" + str(version) + ".0a1&version=" + str(version) + ".0&version=" + str(version) +".0b"
     res = requests.get(urlStr + dateStr + verStr + "&signature=~" + sig)
     jRes = json.loads(res.text)
-    print jRes['total']
     return jRes['total']
 
 def LoadFiles():
-    dates = {}
-
-    with open('result.json', 'r') as fp:
-        lines = fp.readlines()
-        lineNum = len(lines)
-        curLine = 0
-        while curLine < lineNum:
-            date = lines[curLine]
-            date = date.replace('\n','')
-            print date
-            curLine+=1
-            dateNum = int(lines[curLine])
-            curLine+=1
-            localMap = {}
-            for i in range(dateNum):
-                localMap[int(lines[curLine])] = float(lines[curLine+1])
-                curLine+=2
-            dates[date] = localMap
-    dates = collections.OrderedDict(sorted(dates.items()))
-    print dates
-    return dates
-
-def UpdateFiles(dates):
-    with open('result.json', 'w') as fp:
-        for date in dates:
-            fp.write(date + '\n')
-            fp.write(str(len(dates[date])) + '\n')
-            for version in dates[date]:
-                fp.write(str(version) + '\n')
-                fp.write(str(dates[date][version]) + '\n')
-    fp.close()
-    return
+    result = {}
+    result = json.load(open('json/crashLine.json', 'r'))
+    return result
 
 def ShowCrashHistogram():
-    dates = LoadFiles()
     date = datetime.datetime.now().date()
+    result = LoadFiles()
 
-    for i in range(1,30):
+    for i in range(1,16):
         date = date - datetime.timedelta(1)
-        localDict = {}
-        if str(date) in dates:
-            continue
         for version in range(latestVersion - 3, latestVersion+1):
-            # GetCrashNum in date with version
-            # GetTDRNum in date with version
-            localDict[version] = (float(GetTDRNum(date, version)) / float(GetCrashNum(date, version))) * 100
-            #localDict[version] = GetTDRNum(date, version)
-        dates[str(date)] = localDict
-
-    dates = collections.OrderedDict(sorted(dates.items()))
-    for date in dates:
-        print str(date)
-        dates[date] = collections.OrderedDict(sorted(dates[date].items()))
-        for version in dates[date]:
-            print str(version) + " : " + str(dates[date][version])
-
-    SaveJson(dates, "crashLine.json")
-    UpdateFiles(dates)
-    DrawLineGraph(dates, 2)
+            if str(version) not in result:
+                result[str(version)] = {}
+            if str(date) in result[str(version)]:
+                continue
+            crashNum = float(GetCrashNum(date, version))
+            if crashNum == 0:
+                result[str(version)][str(date)] = 0
+            else:
+                result[str(version)][str(date)] = (float(GetTDRNum(date, version)) / crashNum) * 100
+    SaveJson(result, "crashLine.json")
 
 def FetchCrashReports():
     crashReports = {}
@@ -191,40 +80,8 @@ def FetchCrashReports():
             sig['total'] = GetSigNum(version, sig['term'])
     SaveJson(crashReports, "crashReports.json")
 
-
-class ShowLine(Thread):
-    def __init__(self, val):
-        Thread.__init__(self)
-        self.val = val
-
-    def run(self):
-        ShowLineGraph()
-
-class ShowCrashLine(Thread):
-    def __init__(self, val):
-        Thread.__init__(self)
-        self.val = val
-
-    def run(self):
-        ShowCrashHistogram()
-
 def main():
-    # show crash rate
-    # ShowCrashRate()
-    # show pie chart according to version
-    # ShowTDRPieChart()
-    # show histogram
-    #ShowLineGraph()
-    thread1 = ShowLine(1)
-    thread1.start()
-    # show crash histogram
-    thread2 = ShowCrashLine(2)
-    thread2.start()
-    #ShowCrashHistogram()
-
-    thread1.join()
-    thread2.join()
-
+    ShowCrashHistogram()
     FetchCrashReports()
 if __name__ == "__main__" :
     sys.exit(main())
